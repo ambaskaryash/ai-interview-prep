03a8904721df648fd3f6c52c70830035b2342dd3
ambaskaryash <kimetsu119@gmail.com>
2025-08-30 15:43:02 +0530

    refactor: update database URL configuration to use environment variable for PostgreSQL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,3 @@
-import { env } from "@/data/env/server"
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
@@ -6,6 +5,6 @@ export default defineConfig({
   schema: "./src/drizzle/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.POSTGRES_URL || "", // Ensure it's always a string
   },
 })
