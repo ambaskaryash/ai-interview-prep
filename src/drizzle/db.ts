50a4b7405f84cda2e705bf8db48b06b65d0136ca
ambaskaryash <kimetsu119@gmail.com>
2025-08-30 15:45:33 +0530

    fix: ensure DATABASE_URL defaults to an empty string if undefined

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,5 +1,2 @@
-import { env } from "@/data/env/server"
-import { drizzle } from "drizzle-orm/node-postgres"
-import * as schema from "@/drizzle/schema"
 
-export const db = drizzle(env.DATABASE_URL, { schema })
+export const db = drizzle(env.DATABASE_URL || "", { schema })
